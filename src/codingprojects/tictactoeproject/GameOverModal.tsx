1be0e00cc1f530c610fea434bc0d34cf45c9d6ae
rosebudtea <153685901+rosebudtea@users.noreply.github.com>
2024-01-24 23:49:25 +0000

    Got the dialog box styled and working for tictactoe

--- src/codingprojects/tictactoeproject/GameOverModal.tsx
+++ src/codingprojects/tictactoeproject/GameOverModal.tsx
@@ -5,12 +5,14 @@ type GameOverModalProps = { winner: string; onSelect: () => void };
 const GameOverModal = forwardRef(
   (props: GameOverModalProps, ref: ForwardedRef<HTMLDialogElement>) => {
     return (
-      <dialog ref={ref}>
-        {props.winner && <h2>{props.winner} won!</h2>}
-        {!props.winner && <h2>It's a draw!</h2>}
-        <form method="dialog">
-          <button onClick={props.onSelect}>Rematch!</button>
-        </form>
+      <dialog id="container" open={false} ref={ref}>
+        <div id="game-over-modal">
+          {props.winner && <h2>{props.winner} won!</h2>}
+          {!props.winner && <h2>It's a draw!</h2>}
+          <form method="dialog">
+            <button onClick={props.onSelect}>REMATCH!</button>
+          </form>
+        </div>
       </dialog>
     );
   },
